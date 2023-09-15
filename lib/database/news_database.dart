9e3f7bd22a01b1db306d8fb152fd7758c4819bd9
OmarAlgammal <omarprogramzone@gmail.com>
2023-09-15 04:34:20 +0300

    Refactor NewsDatabase class

--- lib/database/news_database.dart
+++ lib/database/news_database.dart
@@ -1,11 +1,10 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:the_daily_journal/core/network/api_constance.dart';
-import 'package:the_daily_journal/services/news_services.dart';
+import 'package:the_daily_journal/services/news_service.dart';
 
 import '../core/network/erorrs/server_failure.dart';
 import '../models/news_model.dart';
-import '../services/news_service.dart';
 
 abstract class BaseNewsDatabase{
   Future<Either<ServerFailure, List<NewsModel>>> getQueryNews({String? query, String? sort});
@@ -18,23 +17,24 @@ abstract class BaseNewsDatabase{
 
 class NewsDatabase implements BaseNewsDatabase{
 
-  final BaseNewsService _baseNewsApiServices;
+  final BaseNewsService _baseNewsApiService;
 
-  NewsDatabase(this._baseNewsApiServices);
+  NewsDatabase(this._baseNewsApiService);
 
   @override
   Future<Either<ServerFailure, List<NewsModel>>> getQueryNews({String? query, String? sort}) async{
-    return await _baseNewsApiServices.getQueryNews(query: query, sort: sort);
+    return await _baseNewsApiService.getData<List<NewsModel>>(path: ApiConstance.allNewsPath(query: query, sort: sort), builder: (List<Map<String, dynamic>> maps) => maps.map((e) => NewsModel.fromJson(e)).toList());
   }
 
   @override
   Future<Either<ServerFailure, List<NewsModel>>> getTopCategoryHeadlines(String category) async{
-    return await _baseNewsApiServices.getTopHeadlines(ApiConstance.topCategoryHeadlinesPath(category));
+        return await _baseNewsApiService.getData<List<NewsModel>>(path: ApiConstance.topCategoryHeadlinesPath(category), builder: (List<Map<String, dynamic>> maps) => maps.map((e) => NewsModel.fromJson(e)).toList());
   }
 
   @override
   Future<Either<ServerFailure, List<NewsModel>>> getTopCountryHeadlines(String countryCode, int? pageSize) async{
-    return await _baseNewsApiServices.getTopHeadlines(ApiConstance.topCountryHeadlinesPath(countryCode, pageSize));
+        return await _baseNewsApiService.getData<List<NewsModel>>(path: ApiConstance.topCountryHeadlinesPath(countryCode, pageSize), builder: (List<Map<String, dynamic>> maps) => maps.map((e) => NewsModel.fromJson(e)).toList(),);
+    
   }
 
 
--- lib/services_locator/services_locator.dart
+++ lib/services_locator/services_locator.dart
@@ -1,20 +1,27 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:the_daily_journal/database/local_database.dart';
 import 'package:the_daily_journal/database/news_database.dart';
 import 'package:the_daily_journal/services/firebase_auth_service.dart';
 import 'package:the_daily_journal/services/local_services.dart';
-import 'package:the_daily_journal/services/news_services.dart';
+import 'package:the_daily_journal/services/news_service.dart';
 import 'package:the_daily_journal/view_model/auth_cubit/auth_cubit.dart';
 import 'package:the_daily_journal/view_model/bookmarks_cubit/bookmark_cubit.dart';
 import 'package:the_daily_journal/view_model/categories_news_cubit/categories_cubit.dart';
 import 'package:the_daily_journal/view_model/query_news_cubit/query_news_cubit.dart';
 
+import '../core/network/api_constance.dart';
+
 GetIt sl = GetIt.instance;
 
 void setup() {
   sl.registerSingleton(FirebaseAuthentication());
-  sl.registerSingleton(NewsServices());
-  sl.registerSingleton(NewsDatabase(sl<NewsServices>()));
+  sl.registerLazySingleton(() => Dio(BaseOptions(
+        baseUrl: ApiConstance.baseUrl,
+      ),
+    ));
+  sl.registerSingleton(NewsService(sl()));
+  sl.registerSingleton(NewsDatabase(sl<NewsService>()));
   sl.registerLazySingleton(() => LocalServices());
   sl.registerLazySingleton(() => LocalDatabase(sl<LocalServices>()));
 
